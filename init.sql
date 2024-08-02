f46a1fdb53b783d7fcc0a93b4a10c51f48de731e
Maxim Kiselev <spl33t@ya.ru>
2024-08-02 17:16:11 +0300

    prisma added

--- init.sql
+++ init.sql
@@ -1,1 +1,11 @@
-CREATE DATABASE ams;
+-- Ensure dblink extension is available
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+-- Conditionally create the database
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ams') THEN
+      PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE ams');
+   END IF;
+END
+$$;
